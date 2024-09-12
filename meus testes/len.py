1b2b7e6d3bafcf29f969ed436fcc08e77e472aa3
jon7346 <jonathanfelipe7346@gmail.com>
2024-09-12 14:56:48 -0300

    Initial commit

---
+++ meus testes/len.py
@@ -0,0 +1,12 @@
+um = 'rosto'
+i = 0 
+while i < len(um):
+ letra = um[i]
+ i += 1
+ print(letra)
+
+
+'''
+ a variavel um possui a palavra rosto armazenada, queremos citar cada uma de suas letras utilizando o codigo, para tal utilzamos uma variavel i em um laço while, criando uma estrutura de repetição
+ nessa estrutura definimos com ' i < len(um):' para devinir que o laço ocorrerá até que a variavel i seja maior que o numero de letras 
+'''
