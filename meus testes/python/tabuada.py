a164871dcca16f01a5d8aef11848658460baa5e4
jon7346 <jonathanfelipe7346@gmail.com>
2024-10-18 00:31:56 +0000

    s

--- meus testes/python/tabuada.py
+++ meus testes/python/tabuada.py
@@ -1,5 +1,6 @@
-tabuada = int(input(print('insira o numero que representa a tabuada a ser inserida: ')))
-
-for range(1,10)
-
-print()
+numero = int(input('insira o numero que representa a tabuada a ser inserida: '))
+resultado = 0 
+for x in range(11):
+    x = +1 
+    resultado = f(x'X'numero;"=" numero * x;) 
+ 
