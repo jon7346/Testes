d2f4cdba2be5de6deafe33069499a239ad018bc2
jon7346 <jonathanfelipe7346@gmail.com>
2025-01-24 15:53:49 -0300

    s

---
+++ sites(web.dev)/funções_javascript.js
@@ -0,0 +1,16 @@
+parseInt('40',10) //retorna 40 
+parcefloat("3.14")//retorna 3.14 
+isNaN('jona')//verifica se é não é um numero 
+isFinite()//verifica se o valor é um numero finito 
+encodeURI()//codifica uma url 
+decodeURI()//decodifica uma url
+eval()//realiza funções simples 
+'String'.charAt()//retorna uma letra especifica dentro da string anterior do ponto 
+"String".concat()//junta strings 
+"String".split()//separa a sting por caracteres em uma array
+"string".replace("s",'j')//substitui um caracter (primeiro ) pelo segundo   
+['array'].push()//adiciona elementos em array 
+['array'].pop()//retorna o ultimo item adicionado e o remove 
+['array'].shift()//retorna o primeiro item da array
+['1','2','3'].map(x => * 2 ) //cria um função para criar um novo array utilizando uma função como base para o novo
+['1','2','3'].filter(x => x > 1) //retorna ['2','3']
--- sites(web.dev)/projeto-to-do/javascrip.js
+++ sites(web.dev)/projeto-to-do/javascrip.js
@@ -1,9 +1,18 @@
-const multer = require('multer')  
+//passo1 importar
 const express = require('express')
-const http =require('http')
-const fs = require("fs")
-            
-function  enviar(){
-    let nome = 
-    let senha =
-}
+const http = require('http')
+const fs = require('fs')
+const multer = require('multer')
+const path = require('path')
+// passo2 criar as funçoes essenciais 
+const PORT = 8080
+const app = express()
+  
+
+app.get('/', (req, res) => {
+res.send(`
+        <h1>to do</h1>
+        <textarea></textarea>
+        <input type = 'button'>enviar <input>`);
+});
+app.post 
