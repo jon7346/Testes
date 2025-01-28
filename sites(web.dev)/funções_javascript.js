e46b26882295932581d8b88cd1838a33f9dabd21
jon7346 <jonathanfelipe7346@gmail.com>
2025-01-28 14:40:13 -0300

    s

--- sites(web.dev)/funções_javascript.js
+++ sites(web.dev)/funções_javascript.js
@@ -14,3 +14,12 @@ eval()//realiza funções simples
 ['array'].shift()//retorna o primeiro item da array
 ['1','2','3'].map(x => * 2 ) //cria um função para criar um novo array utilizando uma função como base para o novo
 ['1','2','3'].filter(x => x > 1) //retorna ['2','3']
+Array.prototype.filter(callback)//Retorna um novo array com elementos que passam no teste da função
+
+Math.abs(value)// retorna o valor absoluto 
+math.abs(-5)//retorna 5 
+math.ceil(value)//arredonda pra cima 
+math.ceil(4.2) //retorna 5 
+math.floor(value)//arredonda pra baixo
+math.floor(4.8)//retorna 4 
+math.random()//retorna um numero aleatorio entre um 
