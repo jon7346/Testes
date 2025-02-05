d2c991eb6ae46f991af71b32eaca7295902ccf4d
jon7346 <jonathanfelipe7346@gmail.com>
2025-02-05 13:02:38 -0300

    s

--- python/testes(desafios)/dev.py
+++ python/testes(desafios)/dev.py
@@ -1,3 +1,7 @@
 palavra = input('insira uma palavra: ')
 
 print("é um numero: ", )
+print('é espaço', palavra.isspace())
+print('é um numero: ', palavra.isnumeric())
+print('é alfanumerio', palavra.isalnum())
+print("")
